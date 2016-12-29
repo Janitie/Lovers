21635be8627129f55bdae62d35a617951dfb0296
徐琬璇 <xuwanxuan@magicGirl.local>
2016-12-29 22:12:26 +0800

    Initial Commit

---
+++ Lovers/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Lovers
+//
+//  Created by 徐琬璇 on 2016/12/29.
+//  Copyright © 2016年 noneTobacco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Lovers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lovers
+//
+//  Created by 徐琬璇 on 2016/12/29.
+//  Copyright © 2016年 noneTobacco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
